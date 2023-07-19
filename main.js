e20de02dcfe8912d474283f46a75fc46d017f4ba
Valentin Rubial <rubialvalentin@gmail.com>
2023-07-19 17:53:09 -0300

    Primera entrega

---
+++ main.js
@@ -0,0 +1,31 @@
+// Calcular el Area de un Triangulo
+
+const AreaTriangulo = (base, altura) => {
+   if (base <= 0 || altura <= 0) {
+      return "La base y la altura deben ser mayores que cero.";
+   }
+   return (base * altura) / 2;
+};
+let baseTriangulo = 20;
+let alturaTriangulo = 5;
+let areaTriangulo = AreaTriangulo(baseTriangulo, alturaTriangulo);
+console.log("El área del triángulo es: " + areaTriangulo);
+
+// Todas las Operaciones de un numero que el usuario elija.
+
+const numeros = (num1, num2) => {
+   while (isNaN(num1) || isNaN(num2)) {
+      alert("Estos numeros no son válidos. Por favor, ingrese números válidos.");
+      num1 = parseInt(prompt("Ingrese el primer número nuevamente:"));
+      num2 = parseInt(prompt("Ingrese el segundo número nuevamente:"));
+   }
+   let suma = num1 + num2;
+   let resta = num1 - num2;
+   let multiplicacion = num1 * num2;
+   let division = num1 / num2;
+   alert(`Suma: ${suma}\nResta: ${resta}\nMultiplicación: ${multiplicacion}\nDivisión: ${division}`);
+};
+let num1 = parseInt(prompt("Ingrese el primer número:"));
+let num2 = parseInt(prompt("Ingrese el segundo número:"));
+numeros(numBase, numOperacion);
+
