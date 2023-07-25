d10fb309ddcb3590de829c8c3bf4fa54a699e971
Valentin Rubial <rubialvalentin@gmail.com>
2023-07-25 17:19:09 -0300

    Correcciones

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 // Calcular el Area de un Triangulo
 
-const AreaTriangulo = (base, altura) => {
+const calcularAreaTriangulo = (base, altura) => {
    if (base <= 0 || altura <= 0) {
       return "ERROR. La base y la altura deben ser mayores que cero.";
    }
    return (base * altura) / 2;
 };
-let baseTriangulo = 0;
-let alturaTriangulo = 5;
-let areaTriangulo = AreaTriangulo(baseTriangulo, alturaTriangulo);
-console.log("El área es: " + areaTriangulo);
+
+const baseTriangulo = parseFloat(prompt("Ingrese la base del triángulo:"));
+const alturaTriangulo = parseFloat(prompt("Ingrese la altura del triángulo:"));
+
+const areaTriangulo = calcularAreaTriangulo(baseTriangulo, alturaTriangulo);
+
+console.log("El área del triángulo es: " + areaTriangulo);
+
 
 // Todas las Operaciones de un numero que el usuario elija.
 
-// const numeros = (num1, num2) => {
-//    while (isNaN(num1) || isNaN(num2)) {
-//       alert("Estos numeros no son válidos. Por favor, ingrese números válidos.");
-//       num1 = parseInt(prompt("Ingrese el primer número nuevamente:"));
-//       num2 = parseInt(prompt("Ingrese el segundo número nuevamente:"));
-//    }
-//    let suma = num1 + num2;
-//    let resta = num1 - num2;
-//    let multiplicacion = num1 * num2;
-//    let division = num1 / num2;
-//    alert(`Suma: ${suma}\nResta: ${resta}\nMultiplicación: ${multiplicacion}\nDivisión: ${division}`);
-// };
-// let num1 = parseInt(prompt("Ingrese el primer número:"));
-// let num2 = parseInt(prompt("Ingrese el segundo número:"));
-// numeros(numBase, numOperacion);
+const performOperations = (numBase, numOperacion) => {
+   while (isNaN(numBase) || isNaN(numOperacion)) {
+      alert("Los números ingresados no son válidos. Por favor, ingrese números válidos.");
+      numBase = parseInt(prompt("Ingrese el primer número nuevamente:"));
+      numOperacion = parseInt(prompt("Ingrese el segundo número nuevamente:"));
+   }
+
+   let suma = numBase + numOperacion;
+   let resta = numBase - numOperacion;
+   let multiplicacion = numBase * numOperacion;
+   let division = numBase / numOperacion;
+   alert(`Suma: ${suma}\nResta: ${resta}\nMultiplicación: ${multiplicacion}\nDivisión: ${division}`);
+};
+
+let numBase = parseInt(prompt("Ingrese el primer número:"));
+let numOperacion = parseInt(prompt("Ingrese el segundo número:"));
+
+performOperations(numBase, numOperacion);
+
+
 
